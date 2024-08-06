40dc98d2a46a21dee9089aba027d3bc0e5d30494
sbrown97 <steph.d.brown97@gmail.com>
2024-08-05 22:34:04 -0400

    2017 Day 10 Part 2

--- 2017/Day10/day10.py
+++ 2017/Day10/day10.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 import argparse
+from functools import reduce
 
 def parse_input(path):
     """ read in and parse input data
@@ -9,17 +10,23 @@ def parse_input(path):
     
     with open(path) as f:
         l = f.readlines()[0]
-        l = l.replace('\n','').split(',')
-        lengths = [int(x) for x in l]  
+        l = l.replace('\n','')
 
-    return lengths
+    return l
 
 
 def part1(lengths,max_val):
 
-    cur_pos = 0
-    skip_size = 0
     data = np.arange(max_val+1)
+    l = [int(i) for i in lengths.split(',')]
+
+    data, _, _ = round(data,l,0,0)
+
+    print("Part 1: {x}".format(x=data[0]*data[1]))
+    return
+
+
+def round(data,lengths,cur_pos,skip_size):
 
     for l in lengths:
         # get sublist to reverse
@@ -34,11 +41,25 @@ def part1(lengths,max_val):
         cur_pos %= len(data)
         skip_size += 1
 
-    print("Part 1: {x}".format(x=data[0]*data[1]))
-    return
+    return data, cur_pos, skip_size
 
-def part2(lengths):
 
+def part2(lengths,max_val):
+
+    lengths = [ord(c) for c in lengths] + [17, 31, 73, 47, 23]
+    data = np.arange(max_val+1)
+    cur_pos = 0
+    skip_size = 0
+
+    for i in range(64):
+        data, cur_pos, skip_size = round(data,lengths,cur_pos,skip_size)
+
+    dense_hash = []
+    for i in range(16):
+        dense_hash.append(reduce(lambda x,y: x^y, data[i*16:(i+1)*16]))
+
+    answer = ''.join('%02x'%i for i in dense_hash)
+    print('Part 2: {x}'.format(x=answer))
     return 
 
 
@@ -48,7 +69,7 @@ if __name__ == "__main__":
 
     parser.add_argument('--input_dir', default=os.path.join(os.curdir,'2017\Day10'), type=str)
     parser.add_argument('--file', default='test.txt',type=str)
-    parser.add_argument('--max_val', default=5,type=int)
+    parser.add_argument('--max_val', default=4,type=int)
 
     args = parser.parse_args()
 
@@ -56,5 +77,5 @@ if __name__ == "__main__":
 
     part1(lengths,args.max_val)
 
-    part2(lengths)
+    part2(lengths,args.max_val)
 
---
+++ 2017/test notes.md
@@ -0,0 +1,21 @@
+Day 4
+- Dificulty: 
+- Description: 
+    - part 1: check string for repeated words
+    - part 2: check string for anagrams
+- Coding Topic(s): regex, string parsing, sets, combinations, anagrams (sorting)
+- Notes: i solved part 1 with str.split() and set(). for part 2 i use combinations and sorted lists
+
+Day 10
+- Dificulty: Medium
+- Description: 
+    - part 1: reverse a subset of a list, make sure it wraps around
+    - part 2: convert characters to bytes, add a for loop, XOR all to create a better hashing function
+- Coding Topic(s): list/array indexing, slicing, reversing, modulo, ascii, XOR, for loops
+- Notes: it took me way too long to get part 1, not because it was hard, but because I was trying to remember the most efficient slicing methods. part 2 was a bit tedious but not crazy. interesting to learn a bit more about hashing 
+
+Day 16
+- Dificulty:
+- Description: 
+- Coding Topic(s):
+- Notes: 
