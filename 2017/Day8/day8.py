aae994c778279416cc7aaab28c48fd597236fe7c
sdbrown <sdbrown@lmi.org>
2024-10-19 11:23:06 -0400

    AoC 2017 Day 8

---
+++ 2017/Day8/day8.py
@@ -0,0 +1,66 @@
+import os
+import argparse
+import operator
+from collections import defaultdict
+
+ops = {
+    '>': lambda a,b: operator.gt(a,b),
+    '>=': lambda a,b: operator.ge(a,b),
+    '<': lambda a,b: operator.lt(a,b),
+    '<=': lambda a,b: operator.le(a,b),
+    '==': lambda a,b: operator.eq(a,b),
+    '!=': lambda a,b: operator.ne(a,b),
+    'inc': lambda a,b: operator.add(a,b),
+    'dec': lambda a,b: operator.sub(a,b)
+}
+
+def parse_input(path):
+    """ read in and parse input data
+    returns """
+    
+    data = []
+    with open(path) as f:
+        for l in f:
+            l = l.replace('\n','')
+            row = l.split(' ')
+
+            data.append(row)  
+
+    return data
+
+
+def part1(data):
+
+    registers = defaultdict(int)
+    max_val = 0
+
+    for instruction in data:
+        if ops[instruction[5]](int(registers[instruction[4]]),int(instruction[6])):
+            registers[instruction[0]] = ops[instruction[1]](int(registers[instruction[0]]),int(instruction[2]))
+            if registers[instruction[0]] > max_val:
+                max_val = registers[instruction[0]]
+
+    print(f'Part 1: {max(registers.values())}')
+    print(f'Part 2: {max_val}')
+    return
+
+def part2(data):
+
+    return 
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='')
+
+    parser.add_argument('--input_dir', default=os.path.join(os.curdir,'2017\\Day8'), type=str)
+    parser.add_argument('--file', default='test.txt',type=str)
+
+    args = parser.parse_args()
+
+    data = parse_input(os.path.join(args.input_dir,args.file))
+
+    part1(data)
+
+    part2(data)
+
